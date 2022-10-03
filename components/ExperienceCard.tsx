717e56316d471df485468fd7ceba391a171215da
Jordan <jordan.k.wilia@gmail.com>
2022-10-03 12:39:15 -0700

    completed multiple components: hero, header, about, and background

---
+++ components/ExperienceCard.tsx
@@ -0,0 +1,45 @@
+
+import { motion } from 'framer-motion'
+import React from 'react'
+
+type Props = {}
+
+function ExperienceCard({}: Props) {
+  return (
+    <article className="flex flex-col rounded-lg items-center space-y-7 flex-shrink-0 w-[500px] md:w-[600px] xl:w-[900px] snap-center bg-#[292929] p-10 hover:opacity-100 opacity-40 cursor-pointer transition-opacity duration-200 overflow-hidden">
+        <motion.img 
+            initial={{
+                y: -100,
+                opacity: 0,
+            }} 
+            transition={{
+                duration: 1.2,
+            }}
+            whileInView={{
+                opacity: 1,
+                y: 0,
+            }}
+            viewport={{
+                once: true
+            }}
+            className="w-32 h-32 rounded-full xl:w-[200px] xl:h-[200px] object-cover object-center"
+            src="/logo.jpg"
+        />
+
+        <div className="px-0 md:px-10">
+            <h4 className="text-4xl font-light">Founder</h4>
+            <p className="font-bold text-2xl mt-1">Townhall Meta</p>
+            <p className="uppercase py-5 text-gray-300">January 2022 - August 2022</p>
+
+            <ul className='list-disc space-y-4 ml-5 text-lg'>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+            </ul>
+        </div>
+    </article>
+  )
+}
+
+export default ExperienceCard
