55d84a9d1024fa0fd2e861966938365f6548eb0b
Jordan <jordan.k.wilia@gmail.com>
2022-10-04 15:21:53 -0700

    wip

--- pages/api/getExperience.ts
+++ pages/api/getExperience.ts
@@ -16,5 +16,4 @@ type Data = {
 export default async function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
   const experiences: Experience[] = await sanityClient.fetch(query)  
   res.status(200).json({ experiences })
-  console.log(experiences)
 }
